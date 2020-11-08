09124bab19161cf565ffa5363b3a706faa66c351
Hua Chan Zhuo <huachanzhuo@berkeley.edu>
2020-11-08 07:15:41 +0000

    Finished part 3

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
+
 movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
     	  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
     	  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
@@ -16,7 +31,7 @@ RSpec.describe MoviesController, type: :controller do
   context "Case when there is movie director" do
     it "creates movies" do
       movies.each do |movie|
-        Movie.create(movie)
+        Movie.create!(movie)
       end
     end
     
@@ -24,4 +39,28 @@ RSpec.describe MoviesController, type: :controller do
       MoviesHelper.oddness(2)
     end
   end
+  
+  describe "creates movies" do
+    it "creates movie" do
+      post :create, :movie => {:title => 'Chicken Run', :rating => 'G', :description => "something", :release_date => '21-Jun-2000', :director => 'Nick Park'}
+      expect(response).not_to eq(nil)
+    end
+  end
+  
+  
+#   describe "Find Similar route," do
+#     context "if the movie has a director," do
+
+#       it "assigns @movies" do
+#         @movies = Movie.create(:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000', :director => 'Nick Park')
+#         get :index
+#         expect(assigns(:movies)).to eq([@movie])
+#       end
+      
+#       it "should go to similar movies page" do
+#         get :similar, id: @movie.id
+#         expect(response).to render_template("similar")
+#       end
+#     end
+#   end
 end
